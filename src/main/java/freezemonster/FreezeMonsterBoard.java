48f8e1173ed454dca7194eaf71eff43c2b904aca
Douglas Gomes <douglasgommez@gmail.com>
2020-10-01 13:36:11 -0300

    Included on the Player class its constructor configs

--- src/main/java/freezemonster/FreezeMonsterBoard.java
+++ src/main/java/freezemonster/FreezeMonsterBoard.java
@@ -1,4 +1,39 @@
 package freezemonster;
 
-public class FreezeMonsterBoard {
+import spriteframework.AbstractBoard;
+import spriteframework.GameBoardSpecification;
+import spriteframework.sprite.BadSprite;
+
+import java.util.LinkedList;
+
+public class FreezeMonsterBoard extends AbstractBoard {
+
+    public FreezeMonsterBoard(GameBoardSpecification gameBoardSpecification) {
+        super(gameBoardSpecification);
+    }
+
+    @Override
+    protected LinkedList<BadSprite> createBadSprites() {
+        return null;
+    }
+
+    @Override
+    protected void createPlayers() {
+
+    }
+
+    @Override
+    protected void drawBoard() {
+
+    }
+
+    @Override
+    protected void gameOver() {
+
+    }
+
+    @Override
+    protected void update() {
+
+    }
 }
--- src/main/java/freezemonster/sprite/Player.java
+++ src/main/java/freezemonster/sprite/Player.java
@@ -1,4 +1,54 @@
 package freezemonster.sprite;
 
-public class Player {
+import spriteframework.Player.BasePlayer;
+import spriteframework.listeners.KeyPressedListener;
+import spriteframework.listeners.KeyReleasedListener;
+import spriteframework.sprite.Position;
+
+import java.awt.event.KeyEvent;
+
+public class Player extends BasePlayer {
+    public Player(String playerImagePath, Position playerInitialPosition) {
+        super(playerImagePath, playerInitialPosition);
+        getImageDimensions();
+        setKeyPressedListener(new KeyPressedListener() {
+            @Override
+            public void onKeyPressed(KeyEvent keyEvent) {
+                int key = keyEvent.getKeyCode();
+                if (key == KeyEvent.VK_LEFT) {
+                    moveHorizontalDisplacement(2, LEFT_DIRECTION);
+                }
+                else if (key == KeyEvent.VK_RIGHT) {
+                    moveHorizontalDisplacement(2, RIGHT_DIRECTION);
+                }
+                else if(key == KeyEvent.VK_UP){
+                    moveVerticalDisplacement(2,UP_DIRECTION);
+                }
+                else if(key == KeyEvent.VK_DOWN){
+                    moveVerticalDisplacement(2,DOWN_DIRECTION);
+                }
+            }
+        });
+
+        setKeyReleasedListener(new KeyReleasedListener() {
+            @Override
+            public void onKeyReleased(KeyEvent keyEvent) {
+                int key = keyEvent.getKeyCode();
+                if (key == KeyEvent.VK_LEFT) {
+                    moveHorizontalDisplacement(0, LEFT_DIRECTION);
+                }
+                else if (key == KeyEvent.VK_RIGHT) {
+                    moveHorizontalDisplacement(0, RIGHT_DIRECTION);
+                }
+                else if(key == KeyEvent.VK_UP){
+                    moveVerticalDisplacement(0,UP_DIRECTION);
+                }
+                else if(key == KeyEvent.VK_DOWN){
+                    moveVerticalDisplacement(0,DOWN_DIRECTION);
+                }
+            }
+        });
+    }
+
+
 }
